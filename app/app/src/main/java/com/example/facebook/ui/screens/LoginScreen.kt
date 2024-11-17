9267100169c333bbf3a6e3fc32482284e4fe3681
phamminhquy <pmquy204@gmail.com>
2024-11-17 08:01:57 +0700

    fix:color-scheme

--- app/app/src/main/java/com/example/facebook/ui/screens/LoginScreen.kt
+++ app/app/src/main/java/com/example/facebook/ui/screens/LoginScreen.kt
@@ -14,15 +14,11 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -42,10 +38,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.facebook.ui.FacebookScreen
 import kotlinx.coroutines.launch
 
@@ -68,89 +62,93 @@ fun LoginScreen(userViewModel: UserViewModel = viewModel(factory = UserViewModel
             }
         }
     }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(8.dp)
-            .padding(horizontal = 16.dp)
-            .pointerInput(Unit) {
-                detectTapGestures(onTap = { focusManager.clearFocus() })
-            },
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "Chào mừng quay trở lại!",
-            style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.Bold),
-            textAlign = TextAlign.Left,
-            modifier = Modifier.fillMaxWidth()
-        )
-        Text(
-            text = "Hãy nhập thông tin đăng nhập để truy cập vào tài khoản của bạn",
-            style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold),
-            color = Color.Gray,
-            textAlign = TextAlign.Left,
-            modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth()
-        )
-        HorizontalDivider(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp, bottom = 32.dp),
-            thickness = DividerDefaults.Thickness
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Địa chỉ Email") },
-            placeholder = { Text("Nhập địa chỉ Email tài khoản") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
-        )
 
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Mật khẩu") },
-            placeholder = { Text("Nhập mật khẩu tài khoản") },
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
-        )
-
-        Button(
-            onClick = handleLogin,
+    Scaffold {
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 16.dp)
+                .fillMaxSize()
+                .padding(it)
+                .padding(8.dp)
+                .padding(horizontal = 16.dp)
+                .pointerInput(Unit) {
+                    detectTapGestures(onTap = { focusManager.clearFocus() })
+                },
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Text("Đăng nhập")
-        }
-
-        Text(
-            text = "Quên mật khẩu?",
-            color = Color.Blue,
-            textAlign = TextAlign.Right,
-            modifier = Modifier
-                .clickable { /* Handle forgot password action */ }
-                .padding(bottom = 32.dp)
-                .fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(96.dp))
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text = "Chưa có tài khoản?", color = Color.Gray)
-            Spacer(modifier = Modifier.width(4.dp))
             Text(
-                text = "Đăng ký ngay",
-                color = Color.Blue,
-                modifier = Modifier.clickable {
-                    // Navigate to the Sign Up screen
-                    navController.navigate(FacebookScreen.SIGNUP.name)
-                }
+                text = "Chào mừng quay trở lại!",
+                style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.Bold),
+                textAlign = TextAlign.Left,
+                modifier = Modifier.fillMaxWidth()
             )
+            Text(
+                text = "Hãy nhập thông tin đăng nhập để truy cập vào tài khoản của bạn",
+                style = TextStyle(fontSize = 16.sp, fontWeight = FontWeight.Bold),
+                color = Color.Gray,
+                textAlign = TextAlign.Left,
+                modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth()
+            )
+            HorizontalDivider(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp, bottom = 32.dp),
+                thickness = DividerDefaults.Thickness
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Địa chỉ Email") },
+                placeholder = { Text("Nhập địa chỉ Email tài khoản") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+            )
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Mật khẩu") },
+                placeholder = { Text("Nhập mật khẩu tài khoản") },
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+            )
+
+            Button(
+                onClick = handleLogin,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp)
+            ) {
+                Text("Đăng nhập")
+            }
+
+            Text(
+                text = "Quên mật khẩu?",
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Right,
+                modifier = Modifier
+                    .clickable { /* Handle forgot password action */ }
+                    .padding(bottom = 32.dp)
+                    .fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(96.dp))
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Chưa có tài khoản?")
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "Đăng ký ngay",
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.clickable {
+                        // Navigate to the Sign Up screen
+                        navController.navigate(FacebookScreen.SIGNUP.name)
+                    }
+                )
+            }
         }
     }
 }
--- app/app/src/main/java/com/example/facebook/ui/screens/RegisterScreen.kt
+++ app/app/src/main/java/com/example/facebook/ui/screens/RegisterScreen.kt
@@ -6,11 +6,31 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,7 +49,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.example.facebook.ui.FacebookScreen
 import com.example.facebook.ui.components.ImagePicker
@@ -50,165 +69,167 @@ fun SignUpScreen(
         coroutineScope.launch {
             try {
                 registerViewModel.otp()
-                navController.navigate(FacebookScreen.HOME.name)
+                navController.navigate(FacebookScreen.LOGIN.name)
             } catch (e: Exception) {
                 Toast.makeText(context, e.message, Toast.LENGTH_SHORT).show()
             }
         }
     }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-            .pointerInput(Unit) {
-                detectTapGestures(onTap = { focusManager.clearFocus() })
-            },
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        if (uiState.showOtpPage) {
-            Text(
-                text = "Xác Thực Tài Khoản",
-                style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.Bold),
-                textAlign = TextAlign.Left,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 0.dp)
-            )
-            Text(
-                text = "Nhập mã OTP đã được gửi đến email của bạn",
-                style = TextStyle(fontSize = 16.sp),
-                color = Color.Gray,
-                textAlign = TextAlign.Left,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 32.dp)
-            )
-            OutlinedTextField(
-                value = uiState.otp,
-                onValueChange = registerViewModel.setOtp,
-                label = { Text("Mã OTP") },
-                placeholder = { Text("Nhập mã OTP") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
-            )
-            Button(
-                onClick = handleVerifyOtp,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 16.dp)
-            ) {
-                Text(text = "Xác nhận OTP")
-            }
-        } else if (uiState.currentStep == 1) {
-            Text(
-                text = "Bắt Đầu Sử Dụng Ngay",
-                style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.Bold),
-                textAlign = TextAlign.Left,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 4.dp)
-            )
-            Text(
-                text = "ZoLA trò chuyện mọi nơi",
-                style = TextStyle(fontSize = 16.sp),
-                color = Color.Gray,
-                textAlign = TextAlign.Right,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp)
-            )
-            HorizontalDivider(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
-                thickness = DividerDefaults.Thickness
-            )
-            OutlinedTextField(
-                value = uiState.firstName,
-                onValueChange = registerViewModel.setFirstName,
-                label = { Text("Tên người dùng") },
-                placeholder = { Text("Nhập tên người dùng") },
-                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
-                textStyle = TextStyle(fontSize = 16.sp, color = Color.Black)
-            )
 
-            OutlinedTextField(
-                value = uiState.lastName,
-                onValueChange = registerViewModel.setLastName,
-                label = { Text("Họ và tên đệm") },
-                placeholder = { Text("Nhập họ và tên đệm người dùng") },
-                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
-                textStyle = TextStyle(fontSize = 16.sp, color = Color.Black)
-            )
-
-            OutlinedTextField(
-                value = uiState.phoneNumber,
-                onValueChange = registerViewModel.setPhoneNumber,
-                label = { Text("Số điện thoại") },
-                placeholder = { Text("Nhập số điện thoại người dùng") },
-                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
-                textStyle = TextStyle(fontSize = 16.sp, color = Color.Black)
-            )
-
-            OutlinedTextField(
-                value = uiState.email,
-                onValueChange = registerViewModel.setEmail,
-                label = { Text("Địa chỉ Email") },
-                placeholder = { Text("Nhập địa chỉ Email tài khoản") },
-                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
-                textStyle = TextStyle(fontSize = 16.sp, color = Color.Black)
-            )
-
-            OutlinedTextField(
-                value = uiState.password,
-                onValueChange = registerViewModel.setPassword,
-                label = { Text("Mật khẩu") },
-                placeholder = { Text("Nhập mật khẩu tài khoản") },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
-            )
-            Text(
-                text = "Mật khẩu cần tối thiểu 8 ký tự",
-                style = TextStyle(fontSize = 12.sp, color = Color.Gray),
-                modifier = Modifier.align(Alignment.Start).padding(start = 8.dp, bottom = 8.dp)
-            )
-            OutlinedTextField(
-                value = uiState.confirmPassword,
-                onValueChange = registerViewModel.setConfirmPassword,
-                label = { Text("Xác nhận mật khẩu") },
-                placeholder = { Text("Nhập lại mật khẩu") },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
-            )
-            Button(
-                onClick = { registerViewModel.setCurrentStep(2) },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 16.dp)
-            ) {
-                Text(text = "Tiếp theo")
-            }
-            Row(
-                horizontalArrangement = Arrangement.Center,
-                modifier = Modifier.fillMaxWidth().padding(top = 64.dp)
-            ) {
-                Text(text = "Đã có tài khoản?", color = Color.Gray)
-                Spacer(modifier = Modifier.width(4.dp))
+    Scaffold {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState())
+                .pointerInput(Unit) {
+                    detectTapGestures(onTap = { focusManager.clearFocus() })
+                },
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            if (uiState.showOtpPage) {
                 Text(
-                    text = "Đăng nhập ngay",
-                    color = Color.Blue,
-                    modifier = Modifier.clickable {
-                        navController.navigate(FacebookScreen.LOGIN.name)
-                    }
+                    text = "Xác Thực Tài Khoản",
+                    style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.Bold),
+                    textAlign = TextAlign.Left,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 0.dp)
                 )
+                Text(
+                    text = "Nhập mã OTP đã được gửi đến email của bạn",
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.Gray,
+                    textAlign = TextAlign.Left,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 32.dp)
+                )
+                OutlinedTextField(
+                    value = uiState.otp,
+                    onValueChange = registerViewModel.setOtp,
+                    label = { Text("Mã OTP") },
+                    placeholder = { Text("Nhập mã OTP") },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+                )
+                Button(
+                    onClick = handleVerifyOtp,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp)
+                ) {
+                    Text(text = "Xác nhận OTP")
+                }
+            } else if (uiState.currentStep == 1) {
+                Text(
+                    text = "Bắt Đầu Sử Dụng Ngay",
+                    style = TextStyle(fontSize = 32.sp, fontWeight = FontWeight.Bold),
+                    textAlign = TextAlign.Left,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 4.dp)
+                )
+                Text(
+                    text = "ZoLA trò chuyện mọi nơi",
+                    style = TextStyle(fontSize = 16.sp),
+                    color = Color.Gray,
+                    textAlign = TextAlign.Right,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp)
+                )
+                HorizontalDivider(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    thickness = DividerDefaults.Thickness
+                )
+                OutlinedTextField(
+                    value = uiState.firstName,
+                    onValueChange = registerViewModel.setFirstName,
+                    label = { Text("Tên người dùng") },
+                    placeholder = { Text("Nhập tên người dùng") },
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
+                )
+
+                OutlinedTextField(
+                    value = uiState.lastName,
+                    onValueChange = registerViewModel.setLastName,
+                    label = { Text("Họ và tên đệm") },
+                    placeholder = { Text("Nhập họ và tên đệm người dùng") },
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
+                )
+
+                OutlinedTextField(
+                    value = uiState.phoneNumber,
+                    onValueChange = registerViewModel.setPhoneNumber,
+                    label = { Text("Số điện thoại") },
+                    placeholder = { Text("Nhập số điện thoại người dùng") },
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
+                )
+
+                OutlinedTextField(
+                    value = uiState.email,
+                    onValueChange = registerViewModel.setEmail,
+                    label = { Text("Địa chỉ Email") },
+                    placeholder = { Text("Nhập địa chỉ Email tài khoản") },
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp),
+                )
+
+                OutlinedTextField(
+                    value = uiState.password,
+                    onValueChange = registerViewModel.setPassword,
+                    label = { Text("Mật khẩu") },
+                    placeholder = { Text("Nhập mật khẩu tài khoản") },
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+                )
+                Text(
+                    text = "Mật khẩu cần tối thiểu 8 ký tự",
+                    style = TextStyle(fontSize = 12.sp, color = Color.Gray),
+                    modifier = Modifier.align(Alignment.Start).padding(start = 8.dp, bottom = 8.dp)
+                )
+                OutlinedTextField(
+                    value = uiState.confirmPassword,
+                    onValueChange = registerViewModel.setConfirmPassword,
+                    label = { Text("Xác nhận mật khẩu") },
+                    placeholder = { Text("Nhập lại mật khẩu") },
+                    visualTransformation = PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+                )
+                Button(
+                    onClick = { registerViewModel.setCurrentStep(2) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp)
+                ) {
+                    Text(text = "Tiếp theo")
+                }
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxWidth().padding(top = 32.dp)
+                ) {
+                    Text(text = "Đã có tài khoản?")
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = "Đăng nhập ngay",
+                        color = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.clickable {
+                            navController.navigate(FacebookScreen.LOGIN.name)
+                        }
+                    )
+                }
+            } else {
+                AvatarSelection(registerViewModel)
             }
-        } else {
-            AvatarSelection(registerViewModel)
         }
     }
+
 }
 @RequiresApi(Build.VERSION_CODES.Q)
 @Composable

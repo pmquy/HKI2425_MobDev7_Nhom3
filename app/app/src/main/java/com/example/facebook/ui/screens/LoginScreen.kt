6be38e3d81911b21e3e0edc779a4826f276ef29c
ThienND04 <nguyenducthien808@gmail.com>
2024-12-15 16:14:56 +0700

    fix + logo

--- app/app/src/main/java/com/example/facebook/ui/screens/LoginScreen.kt
+++ app/app/src/main/java/com/example/facebook/ui/screens/LoginScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults
@@ -28,12 +29,14 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
@@ -107,15 +110,25 @@ fun LoginScreen(userViewModel: UserViewModel = viewModel(factory = UserViewModel
             OutlinedTextField(
                 value = email,
                 onValueChange = { email = it },
+                singleLine = true,
                 label = { Text("Địa chỉ Email") },
                 placeholder = { Text("Nhập địa chỉ Email tài khoản") },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = {
+                        focusManager.moveFocus(FocusDirection.Next)
+                    }
+                ),
                 modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
             )
 
             OutlinedTextField(
                 value = password,
                 onValueChange = { password = it },
+                singleLine = true,
                 label = { Text("Mật khẩu") },
                 placeholder = { Text("Nhập mật khẩu tài khoản") },
                 visualTransformation = PasswordVisualTransformation(),
